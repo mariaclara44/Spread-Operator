c0e35cdd7f3e703699a1a5cd1932ebeeeb44f87f
gustavo <gustavo.sotana@edu.senai.br>
2026-06-12 08:08:39 -0300

    Feat: Finaliza a atividade

--- atividade.js
+++ atividade.js
@@ -1,5 +1,4 @@
 // 1️⃣Copiando um Array
-
 const numeros = [1, 2, 3];
 const copia = [...numeros];
 console.log(copia);
@@ -10,7 +9,6 @@ const adicionaFruta = [...frutas, 'Pessêgo' ];
 console.log(adicionaFruta);
 
 //3️⃣Combinando Dois Arrays
-
 const number = [1, 2, 3];
 const number1 = [2, 4, 5];
 const combinaArray = [...number, ...number1];
@@ -44,3 +42,50 @@ const objeto = {nome: 'celular', marca: 'Samsumg' };
 console.log(propriedadeAdicionada(objeto, 'ano', 2023 ));
 
 // 7️⃣Carrinho de Compras
+const aluno = {
+ nome: "Lucas",
+ idade: 17
+}
+
+const alunoAtualizado = {
+    ...aluno, 
+    curso: 'Desenvolvimento de sistemas'
+}
+
+console.log(alunoAtualizado); 
+
+// 8️⃣  Alterando propriedade
+const produto = {
+ nome: "Notebook",
+ preco: 3000
+}
+
+const produtoAtualizado = {
+    ...produto, 
+    preco: 3500
+}
+
+console.log(produtoAtualizado);
+
+// 9️⃣ Simulando lista de tarefas
+const tarefas1 = [];
+
+const tarefas2 = [...tarefas1, 'Estudar JS']
+const tarefas3 = [...tarefas2, 'Fazer exercício']
+const tarefas4 = [...tarefas3, 'Dormir cedo']
+
+console.log(tarefas4);
+
+// 🔟 Lista de compras
+const compras1 = [...compras, 'Macarrão']
+const compras2 = [...compras1, 'Leite']
+const compras3 = [...compras2, 'Ovos']
+
+console.log(compras3);
+
+// Desafio Final (nível técnico)
+let listaDeTarefas = ["Estudar", "Treinar"]
+
+listaDeTarefas = [...listaDeTarefas, 'Programar', 'Assistir Aula']
+
+console.log(listaDeTarefas);
